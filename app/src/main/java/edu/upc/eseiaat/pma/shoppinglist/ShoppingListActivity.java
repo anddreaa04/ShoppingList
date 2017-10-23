bb3e3f73c8d955d3e1def1f6a7683c02961d9d3a
andreaortega <andreaortegadumont@gmail.com>
2017-10-23 15:46:19 +0200

    13) INICI PART 3: s'ha fet fins l'OnStop.

--- app/src/main/java/edu/upc/eseiaat/pma/shoppinglist/ShoppingListActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/shoppinglist/ShoppingListActivity.java
@@ -1,9 +1,11 @@
 package edu.upc.eseiaat.pma.shoppinglist;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Adapter;
@@ -13,10 +15,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class ShoppingListActivity extends AppCompatActivity {
+
+    private static final String FILENAME = "shopping_list.txt";
 
     private ArrayList<Shoppingitem> itemList;
     private ShoppingListAdapter adapter;
@@ -24,6 +33,36 @@ public class ShoppingListActivity extends AppCompatActivity {
     private ListView list;
     private Button btn_add;
     private EditText edit_item;
+
+    private void writeItemList(){
+
+        //GRABAMOS CADA PARTE DE LA LISTA
+
+        String FILENAME = "shopping_list.txt";
+        try {
+            FileOutputStream fos = openFileOutput(FILENAME, Context.MODE_PRIVATE);
+            for (int i=0; i<itemList.size();i++){
+                Shoppingitem it= itemList.get(i);
+                String line= String.format("%s;%b\n", it.getText(),it.isChecked());
+                fos.write(line.getBytes());
+
+            }
+            fos.close();
+        } catch (FileNotFoundException e) {
+            Log.e("andrea","writeItemList:FileNotFoundException");
+            Toast.makeText(this, R.string.cannot_write, Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            Log.e("andrea","writeItemList: IOException");
+            Toast.makeText(this, R.string.cannot_write, Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        writeItemList();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
